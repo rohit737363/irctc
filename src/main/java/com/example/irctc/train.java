f1506d144d53b9acb947ec96967b8d97d5bce02a
rohit737363 <chougalerohit73@gmail.com>
2023-03-17 13:18:13 +0530

    newclass

--- src/main/java/com/example/irctc/train.java
+++ src/main/java/com/example/irctc/train.java
@@ -1,4 +1,13 @@
-package com.example.irctc;
+package com.example.onlineshoopingms;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
 public class train {
+    @GetMapping("/mytrain")
+    public String getData(){
+        return  "Please confirm ticket";
+    }
 }
